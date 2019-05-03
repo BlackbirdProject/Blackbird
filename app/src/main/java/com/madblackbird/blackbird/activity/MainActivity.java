3595eae586d316dd8b4adac6286dd4ca0bcdea76
Julen Bujanda <21701719@live.uem.es>
2019-05-03 17:48:55 +0200

    Initial commit

---
+++ app/src/main/java/com/madblackbird/blackbird/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.madblackbird.blackbird.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.madblackbird.blackbird.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
