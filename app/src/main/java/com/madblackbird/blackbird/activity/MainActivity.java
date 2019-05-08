0eec18c675eec7518feec69c2805ec8748c67e8d
Julen Bujanda <21701719@live.uem.es>
2019-05-08 09:38:14 +0200

    Bottom sheet, Butterknife

--- app/src/main/java/com/madblackbird/blackbird/activity/MainActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.madblackbird.blackbird.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.madblackbird.blackbird.R;
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, TripDetailsActivity.class));
     }
 }
--- app/src/main/java/com/madblackbird/blackbird/activity/TripDetailsActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/TripDetailsActivity.java
@@ -3,24 +3,35 @@ package com.madblackbird.blackbird.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.madblackbird.blackbird.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TripDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap googleMap;
+    private BottomSheetBehavior bottomSheetBehavior;
+
+    @BindView(R.id.bottom_sheet_trip_details)
+    private LinearLayout bottomSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_details);
+        ButterKnife.bind(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.trip_details_map);
         if (mapFragment != null)
             mapFragment.getMapAsync(this);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
     }
 
     @Override
