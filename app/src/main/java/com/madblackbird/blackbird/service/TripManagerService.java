8d227c8ca823e847ee791171aa83a5f17e84a473
Julen Bujanda <21701719@live.uem.es>
2019-05-10 14:34:34 +0200

    Pass context to TripManagerService to get string from resources

--- app/src/main/java/com/madblackbird/blackbird/service/TripManagerService.java
+++ app/src/main/java/com/madblackbird/blackbird/service/TripManagerService.java
@@ -1,9 +1,12 @@
 package com.madblackbird.blackbird.service;
 
+import android.content.Context;
+
 import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.madblackbird.blackbird.R;
 import com.madblackbird.blackbird.callback.OnTripLoadCallback;
 import com.madblackbird.blackbird.dataClasses.TripPlan;
 
@@ -15,7 +18,7 @@ public class TripManagerService {
 
     private static final Gson gson = new Gson();
 
-    public static void findRoute(LatLng from, LatLng to, final OnTripLoadCallback callback) {
+    public static void findRoute(Context context, LatLng from, LatLng to, final OnTripLoadCallback callback) {
         RequestParams requestParams = new RequestParams();
         requestParams.add("fromPlace", from.latitude + ", " + from.longitude);
         requestParams.add("toPlace", to.latitude + ", " + to.longitude);
@@ -24,7 +27,11 @@ public class TripManagerService {
         requestParams.add("mode", "TRANSIT,WALK");
         requestParams.add("maxWalkDistance", "1000");
         requestParams.add("arriveBy", "false");
-        OTPRestApi.get("plan", requestParams, new JsonHttpResponseHandler() {
+        OTPRestApi.get(
+                context.getString(R.string.otp_server_url),
+                "plan",
+                requestParams,
+                new JsonHttpResponseHandler() {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
