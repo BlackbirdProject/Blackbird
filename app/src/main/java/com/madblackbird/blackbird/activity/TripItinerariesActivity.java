7ea4caa3e56bc8b860f79c23a0e4518cf3200a58
Julen Bujanda <21701719@live.uem.es>
2019-05-10 14:33:16 +0200

    Add LinerLayoutManager to RecyclerView

--- app/src/main/java/com/madblackbird/blackbird/activity/TripItinerariesActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/TripItinerariesActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -12,11 +13,13 @@ import com.madblackbird.blackbird.adapter.ItineraryRecyclerViewAdapter;
 import com.madblackbird.blackbird.callback.OnTripLoadCallback;
 import com.madblackbird.blackbird.dataClasses.Itinerary;
 import com.madblackbird.blackbird.dataClasses.Plan;
+import com.madblackbird.blackbird.service.LocationService;
 import com.madblackbird.blackbird.service.TripManagerService;
 
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TripItinerariesActivity extends AppCompatActivity {
 
@@ -29,11 +32,19 @@ public class TripItinerariesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_itineraries);
+        ButterKnife.bind(this);
+        recyclerViewItineraries.setLayoutManager(new LinearLayoutManager(this));
         Intent intent = getIntent();
         double latitude = intent.getDoubleExtra("latitude", 0);
         double longitude = intent.getDoubleExtra("longitude", 0);
+        LocationService locationService = new LocationService(this);
+        LatLng from = locationService.getCurrentLocation();
+        if (from == null)
+            from = new LatLng(40.447216, -3.692497);
         if (latitude != 0 && longitude != 0)
-            TripManagerService.findRoute(new LatLng(40.447216, -3.692497),
+            TripManagerService.findRoute(
+                    this,
+                    from,
                     new LatLng(latitude, longitude),
                     new OnTripLoadCallback() {
                         @Override
