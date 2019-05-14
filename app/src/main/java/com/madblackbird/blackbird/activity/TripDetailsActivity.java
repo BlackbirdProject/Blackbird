85f3e891e14967146183a1a121f569f1d7b7791f
Julen Bujanda <21701719@live.uem.es>
2019-05-14 16:00:31 +0200

    Polyline width

--- app/src/main/java/com/madblackbird/blackbird/activity/TripDetailsActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/TripDetailsActivity.java
@@ -53,7 +53,7 @@ public class TripDetailsActivity extends AppCompatActivity implements OnMapReady
                         color = Color.parseColor("#" + leg.getRouteColor());
                     } catch (Exception ignored) {
                     }
-                    googleMap.addPolyline(new PolylineOptions().addAll(PolyUtil.decode(legGeometry.getPoints())).color(color).width(2));
+                    googleMap.addPolyline(new PolylineOptions().addAll(PolyUtil.decode(legGeometry.getPoints())).color(color));
                 }
             }
         }
