3d1bdece4096b3965a833e8f35005bb109f880d1
Julen Bujanda <21701719@live.uem.es>
2019-05-14 09:18:03 +0200

    OnClickListener, show trip times

--- app/src/main/java/com/madblackbird/blackbird/adapter/ItineraryRecyclerViewAdapter.java
+++ app/src/main/java/com/madblackbird/blackbird/adapter/ItineraryRecyclerViewAdapter.java
@@ -22,6 +22,7 @@ import java.util.List;
 public class ItineraryRecyclerViewAdapter extends RecyclerView.Adapter<ItineraryRecyclerViewAdapter.Holder> {
 
     private List<Itinerary> itineraries;
+    private View.OnClickListener onClickListener;
 
     public ItineraryRecyclerViewAdapter(List<Itinerary> itineraries) {
         this.itineraries = itineraries;
@@ -30,13 +31,24 @@ public class ItineraryRecyclerViewAdapter extends RecyclerView.Adapter<Itinerary
     @NonNull
     @Override
     public Holder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_itinerary, null, false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_itinerary, viewGroup, false);
+        RecyclerView.ViewHolder holder = new Holder(view);
+        if (onClickListener != null)
+            holder.itemView.setOnClickListener(v -> onClickListener.onClick(v));
         return new Holder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
         holder.bind(itineraries.get(position));
+    }
+
+    public void setOnClickListener(View.OnClickListener callback) {
+        onClickListener = callback;
+    }
+
+    public Itinerary getItinerary(int position) {
+        return itineraries.get(position);
     }
 
     @Override
@@ -47,19 +59,24 @@ public class ItineraryRecyclerViewAdapter extends RecyclerView.Adapter<Itinerary
     static class Holder extends RecyclerView.ViewHolder {
 
         private final LinearLayout layoutTransportIcons;
-        private final TextView lblTime;
+        private final TextView lblDuration, lblTime;
 
         private final Context context;
 
         Holder(View view) {
             super(view);
             layoutTransportIcons = view.findViewById(R.id.linear_transport_icons);
+            lblDuration = view.findViewById(R.id.item_itinerary_duration);
             lblTime = view.findViewById(R.id.item_itinerary_time);
             context = view.getContext();
         }
 
         void bind(Itinerary itinerary) {
-            lblTime.setText(formatTime(itinerary.getDuration()));
+            lblDuration.setText(formatDuration(itinerary.getDuration()));
+            String time = DateUtils.formatDateTime(context, itinerary.getStartTime(),
+                    DateUtils.FORMAT_SHOW_TIME) + " > " + DateUtils.formatDateTime(context, itinerary.getEndTime(),
+                    DateUtils.FORMAT_SHOW_TIME);
+            lblTime.setText(time);
             for (Leg leg : itinerary.getLegs()) {
                 Drawable drawable;
                 switch (leg.getMode()) {
@@ -81,11 +98,12 @@ public class ItineraryRecyclerViewAdapter extends RecyclerView.Adapter<Itinerary
                     textView.setTextColor(Color.parseColor("#" + leg.getRouteColor()));
                 ImageView imageView = new ImageView(context);
                 imageView.setImageDrawable(drawable);
+                layoutTransportIcons.addView(imageView);
                 layoutTransportIcons.addView(textView);
             }
         }
 
-        private String formatTime(Integer duration) {
+        private String formatDuration(Integer duration) {
             return duration / 60 + " minutes";
         }
 
