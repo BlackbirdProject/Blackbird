dde8f689708d39a3382ef27835b6793c80d95f92
Julen Bujanda <21701719@live.uem.es>
2019-05-14 18:49:38 +0200

    Bind view to Activity, open PlacesAutoComplete on search box click

--- app/src/main/java/com/madblackbird/blackbird/activity/MainActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/MainActivity.java
@@ -2,70 +2,41 @@ package com.madblackbird.blackbird.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.RectangularBounds;
-import com.google.android.libraries.places.api.net.PlacesClient;
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.madblackbird.blackbird.R;
 
-import java.util.Arrays;
-import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    @BindView(R.id.search_box_places_autocomplete)
+    EditText searchBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map_main_activity);
         if (mapFragment != null)
             mapFragment.getMapAsync(this);
+        searchBox.setOnClickListener(v ->
+                startActivity(new Intent(this, PlacesAutocompleteActivity.class)));
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        if (!Places.isInitialized()) {
-            Places.initialize(this, getString(R.string.google_maps_api_key));
-        }
-        PlacesClient placesClient = Places.createClient(this);
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
-        List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG);
-        assert autocompleteFragment != null;
-        autocompleteFragment.setPlaceFields(placeFields);
-        autocompleteFragment.setCountry("es");
-        autocompleteFragment.setLocationRestriction(RectangularBounds.newInstance(
-                new LatLng(39.938037, -4.815362),
-                new LatLng(41.211806, -2.870480)
-        ));
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(@NonNull Place place) {
-                Intent intent = new Intent(MainActivity.this, TripItinerariesActivity.class);
-                if (place.getLatLng() != null) {
-                    intent.putExtra("latitude", place.getLatLng().latitude);
-                    intent.putExtra("longitude", place.getLatLng().longitude);
-                }
-                startActivity(intent);
-            }
-
-            @Override
-            public void onError(@NonNull Status status) {
-
-            }
-        });
+        this.googleMap = googleMap;
     }
 
 }
