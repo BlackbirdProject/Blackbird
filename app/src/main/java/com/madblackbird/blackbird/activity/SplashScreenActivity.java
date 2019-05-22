d5d114185c98c70f87157c456aa5d46cd5e8f158
Adrian <adri.garcia.universidad@gmail.com>
2019-05-22 21:37:28 +0200

    base splash

--- app/src/main/java/com/madblackbird/blackbird/activity/SplashScreenActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package com.madblackbird.blackbird.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +11,18 @@ import com.madblackbird.blackbird.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        startActivity(new Intent(this, MainActivity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(context, MainActivity.class));
+            }
+        }, 2000);
+
     }
 
 }
