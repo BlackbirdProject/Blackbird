9dbb15c276a0670aaf504102528c75fa2f86a1ff
Julen Bujanda <21701719@live.uem.es>
2019-05-27 15:31:51 +0200

    Itinerart callback

---
+++ app/src/main/java/com/madblackbird/blackbird/callback/OnItineraryLoadCallback.java
@@ -0,0 +1,9 @@
+package com.madblackbird.blackbird.callback;
+
+import com.madblackbird.blackbird.dataClasses.Itinerary;
+
+public interface OnItineraryLoadCallback {
+
+    void onLoad(Itinerary itinerary);
+
+}
