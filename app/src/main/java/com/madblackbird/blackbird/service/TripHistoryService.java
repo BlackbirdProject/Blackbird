8d3704027556dfc8eebb902d7f6843e9ed5871a1
Julen Bujanda <21701719@live.uem.es>
2019-05-27 15:32:29 +0200

    Load and save itineraries

--- app/src/main/java/com/madblackbird/blackbird/service/TripHistoryService.java
+++ app/src/main/java/com/madblackbird/blackbird/service/TripHistoryService.java
@@ -1,24 +1,68 @@
 package com.madblackbird.blackbird.service;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.madblackbird.blackbird.callback.OnItineraryLoadCallback;
 import com.madblackbird.blackbird.dataClasses.Itinerary;
 
 public class TripHistoryService {
 
-    private FirebaseFirestore firebaseFirestore;
+    private FirebaseDatabase firebaseDatabase;
     private FirebaseUser firebaseUser;
 
     public TripHistoryService() {
-        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
     }
 
     public void addTrip(Itinerary itinerary) {
-        firebaseFirestore.collection("savedTrips")
-                .document(firebaseUser.getUid())
-                .set(itinerary);
+        if (firebaseUser != null) {
+            DatabaseReference savedTrips = firebaseDatabase.getReference("savedTrips");
+            savedTrips.child(firebaseUser.getUid())
+                    .setValue(itinerary);
+        }
+    }
+
+    public void getItineraries(OnItineraryLoadCallback callback) {
+        if (firebaseUser != null) {
+            DatabaseReference savedTrips = firebaseDatabase.getReference("savedTrips");
+            savedTrips.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Itinerary itinerary = dataSnapshot.getValue(Itinerary.class);
+                    if (itinerary != null)
+                        callback.onLoad(itinerary);
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 
 }
