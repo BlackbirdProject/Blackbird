65070c9463849ac5cbdfacfd81a2d631615819e1
Julen Bujanda <21701719@live.uem.es>
2019-05-28 16:21:11 +0200

    Draw walking PolyLine

--- app/src/main/java/com/madblackbird/blackbird/activity/TripDetailsActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/TripDetailsActivity.java
@@ -13,10 +13,13 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Dot;
+import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.maps.android.PolyUtil;
 import com.madblackbird.blackbird.R;
 import com.madblackbird.blackbird.adapter.TripDetailsAdapter;
@@ -25,6 +28,7 @@ import com.madblackbird.blackbird.dataClasses.Leg;
 import com.madblackbird.blackbird.dataClasses.LegGeometry;
 import com.madblackbird.blackbird.service.TripManagerService;
 
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -53,7 +57,6 @@ public class TripDetailsActivity extends AppCompatActivity implements OnMapReady
             mapFragment.getMapAsync(this);
         Intent intent = getIntent();
         itinerary = (Itinerary) intent.getSerializableExtra("itinerary");
-        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
     }
 
     @Override
@@ -62,22 +65,43 @@ public class TripDetailsActivity extends AppCompatActivity implements OnMapReady
         googleMap.setOnMapLoadedCallback(this::onMapLoaded);
         if (itinerary != null) {
             for (Leg leg : itinerary.getLegs()) {
-                LegGeometry legGeometry = leg.getLegGeometry();
-                if (legGeometry != null) {
-                    int color = Color.BLACK;
-                    try {
-                        color = Color.parseColor("#" + leg.getRouteColor());
-                    } catch (Exception ignored) {
-                    }
-                    googleMap.addPolyline(new PolylineOptions()
-                            .addAll(PolyUtil.decode(legGeometry.getPoints()))
-                            .color(color)
-                            .width(20));
-                }
+                if (leg.getMode().equals("WALK"))
+                    drawWalkPolyline(leg);
+                else
+                    drawTransitPolyline(leg);
             }
             recyclerViewTripDetails.setLayoutManager(new LinearLayoutManager(this));
             populateRecycleView(itinerary.getLegs());
         }
+    }
+
+    private void drawTransitPolyline(Leg leg) {
+        LegGeometry legGeometry = leg.getLegGeometry();
+        if (legGeometry != null) {
+            int color = Color.BLACK;
+            try {
+                color = Color.parseColor("#" + leg.getRouteColor());
+            } catch (Exception ignored) {
+            }
+            googleMap.addPolyline(new PolylineOptions()
+                    .addAll(PolyUtil.decode(legGeometry.getPoints()))
+                    .color(color)
+                    .width(25));
+        }
+    }
+
+    private void drawWalkPolyline(Leg leg) {
+        LegGeometry legGeometry = leg.getLegGeometry();
+        if (legGeometry != null) {
+            PolylineOptions polyLineOptions = new PolylineOptions();
+            polyLineOptions.addAll(PolyUtil.decode(legGeometry.getPoints()));
+            polyLineOptions.width(25);
+            polyLineOptions.color(Color.rgb(0, 178, 255));
+            Polyline polyline = googleMap.addPolyline(polyLineOptions);
+            List<PatternItem> pattern = Arrays.asList(new Dot(), new Gap(10f));
+            polyline.setPattern(pattern);
+        }
+
     }
 
     public void onMapLoaded() {
