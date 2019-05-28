29ebe54e3f1d9052b12cc35a86d7456e373986fd
julenb <21701719@live.uem.es>
2019-05-28 23:32:55 +0200

    Place load callback

---
+++ app/src/main/java/com/madblackbird/blackbird/callback/OnPlaceLoadCallback.java
@@ -0,0 +1,9 @@
+package com.madblackbird.blackbird.callback;
+
+import com.google.android.libraries.places.api.model.Place;
+
+public interface OnPlaceLoadCallback {
+
+    void onLoad(Place place);
+
+}
