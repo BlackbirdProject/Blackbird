32a98ec9c4f6d449d7423875fc04f8784ca23e47
julenb <21701719@live.uem.es>
2019-05-28 23:33:44 +0200

    Add and get favourite places

--- app/src/main/java/com/madblackbird/blackbird/service/TripDatabaseService.java
+++ app/src/main/java/com/madblackbird/blackbird/service/TripDatabaseService.java
@@ -3,6 +3,7 @@ package com.madblackbird.blackbird.service;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.libraries.places.api.model.Place;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -11,14 +12,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.madblackbird.blackbird.callback.OnItineraryLoadCallback;
+import com.madblackbird.blackbird.callback.OnPlaceLoadCallback;
 import com.madblackbird.blackbird.dataClasses.Itinerary;
 
-public class TripHistoryService {
+public class TripDatabaseService {
 
     private FirebaseDatabase firebaseDatabase;
     private FirebaseUser firebaseUser;
 
-    public TripHistoryService() {
+    public TripDatabaseService() {
         firebaseDatabase = FirebaseDatabase.getInstance();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
     }
@@ -28,6 +30,48 @@ public class TripHistoryService {
             DatabaseReference savedTrips = firebaseDatabase.getReference("savedTrips");
             savedTrips.child(firebaseUser.getUid())
                     .setValue(itinerary);
+        }
+    }
+
+    public void addFavourite(Place place) {
+        if (firebaseUser != null) {
+            DatabaseReference favouritePlaces = firebaseDatabase.getReference("favouritePlaces");
+            favouritePlaces.child(firebaseUser.getUid())
+                    .setPriority(place);
+        }
+    }
+
+    public void getFavourites(OnPlaceLoadCallback callback) {
+        if (firebaseUser != null) {
+            DatabaseReference favouritePlaces = firebaseDatabase.getReference("favouritePlaces");
+            favouritePlaces.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Place place = dataSnapshot.getValue(Place.class);
+                    if (place != null)
+                        callback.onLoad(place);
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
         }
     }
 
