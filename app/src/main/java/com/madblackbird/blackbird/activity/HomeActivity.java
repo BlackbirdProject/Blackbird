e521edf76e3c2af8e7734fbe15390ebdf578ffac
julenb <21701719@live.uem.es>
2019-05-28 22:20:27 +0200

    Check for user

--- app/src/main/java/com/madblackbird/blackbird/activity/HomeActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/HomeActivity.java
@@ -1,14 +1,15 @@
 package com.madblackbird.blackbird.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.madblackbird.blackbird.R;
 import com.madblackbird.blackbird.fragment.HomeFragment;
 
@@ -16,6 +17,7 @@ public class HomeActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nv);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         initializeUI();
     }
 
@@ -35,10 +38,9 @@ public class HomeActivity extends AppCompatActivity {
                 case R.id.menu_home:
                     transaction.replace(R.id.content_frame, new HomeFragment(), "homeFragment");
                     break;
-                /*case R.id.nav_map:
-                    menuItems.get(R.id.menu_filter).setVisible(false);
-                    menuItems.get(R.id.menu_map).setVisible(true);
-                    transaction.replace(R.id.content_frame, new MapFragment(), "mapFragment");*/
+                case R.id.menu_trip_history:
+                    openLoginRequiredFragment(menuItem.getItemId());
+                    break;
             }
             transaction.addToBackStack(null);
             transaction.commit();
@@ -51,21 +53,10 @@ public class HomeActivity extends AppCompatActivity {
         transaction.commit();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-            case R.id.menu_account:
-                return false;
-            /*case R.id.account:
-                MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentByTag("mapFragment");
-                if (mapFragment != null)
-                    mapFragment.changeMapType();
-                return true;*/
+    private void openLoginRequiredFragment(int itemId) {
+        if (firebaseUser == null) {
+            startActivity(new Intent(this, LoginActivity.class));
         }
-        return super.onOptionsItemSelected(item);
     }
 
 }
