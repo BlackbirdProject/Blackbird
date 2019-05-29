6a89680ce4590f99826a301ccb51864332dac1c8
Julen Bujanda <21701719@live.uem.es>
2019-05-29 13:38:08 +0200

    Favourite destination adapter

---
+++ app/src/main/java/com/madblackbird/blackbird/adapter/FavouriteDestinationAdapter.java
@@ -0,0 +1,72 @@
+package com.madblackbird.blackbird.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.madblackbird.blackbird.R;
+import com.madblackbird.blackbird.dataClasses.OTPPlace;
+
+import java.util.List;
+
+public class FavouriteDestinationAdapter extends RecyclerView.Adapter<FavouriteDestinationAdapter.Holder> {
+
+    private List<OTPPlace> places;
+    private View.OnClickListener onClickListener;
+
+    public FavouriteDestinationAdapter(List<OTPPlace> places) {
+        this.places = places;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_favourite_place, viewGroup, false);
+        RecyclerView.ViewHolder holder = new Holder(view);
+        if (onClickListener != null)
+            holder.itemView.setOnClickListener(v -> onClickListener.onClick(v));
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        holder.bind(places.get(position));
+    }
+
+    public void setOnClickListener(View.OnClickListener callback) {
+        onClickListener = callback;
+    }
+
+    public OTPPlace getPlace(int position) {
+        return places.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return places.size();
+    }
+
+    static class Holder extends RecyclerView.ViewHolder {
+
+        private final TextView lblName;
+
+        private final Context context;
+
+        Holder(View view) {
+            super(view);
+            lblName = view.findViewById(R.id.txt_favourite_place_name);
+            context = view.getContext();
+        }
+
+        void bind(OTPPlace place) {
+            lblName.setText(place.getName());
+        }
+
+    }
+
+}
