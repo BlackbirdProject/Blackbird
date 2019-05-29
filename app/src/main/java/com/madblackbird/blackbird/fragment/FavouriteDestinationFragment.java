f302247a08d738a29f7e562c0066bafb9e374b79
Julen Bujanda <21701719@live.uem.es>
2019-05-29 14:04:30 +0200

    Initialize RecyclerView

--- app/src/main/java/com/madblackbird/blackbird/fragment/FavouriteDestinationFragment.java
+++ app/src/main/java/com/madblackbird/blackbird/fragment/FavouriteDestinationFragment.java
@@ -1,17 +1,35 @@
 package com.madblackbird.blackbird.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.madblackbird.blackbird.R;
+import com.madblackbird.blackbird.adapter.FavouriteDestinationAdapter;
+import com.madblackbird.blackbird.dataClasses.OTPPlace;
+import com.madblackbird.blackbird.service.TripDatabaseService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FavouriteDestinationFragment extends Fragment {
 
+    @BindView(R.id.recycler_view_favourites)
+    RecyclerView recyclerViewFavourites;
+
+    private FavouriteDestinationAdapter favouriteDestinationAdapter;
+    private TripDatabaseService tripDatabaseService;
+    private List<OTPPlace> places;
 
     public FavouriteDestinationFragment() {
 
@@ -21,6 +39,23 @@ public class FavouriteDestinationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_favourite_destination, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, getView());
+        tripDatabaseService = new TripDatabaseService();
+        recyclerViewFavourites.setLayoutManager(new LinearLayoutManager(getContext()));
+        places = new ArrayList<>();
+        favouriteDestinationAdapter = new FavouriteDestinationAdapter(places);
+        recyclerViewFavourites.setAdapter(favouriteDestinationAdapter);
+        tripDatabaseService.getFavourites(this::addFavourite);
+    }
+
+    private void addFavourite(OTPPlace place) {
+        places.add(place);
+        favouriteDestinationAdapter.notifyDataSetChanged();
     }
 
 }
