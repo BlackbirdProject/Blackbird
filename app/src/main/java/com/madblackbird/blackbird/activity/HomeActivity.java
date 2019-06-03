db9e47af5003e9d7cf15254216d34e2ace182a33
Julen Bujanda <21701719@live.uem.es>
2019-06-03 09:35:11 +0200

    Optimize imports

--- app/src/main/java/com/madblackbird/blackbird/activity/HomeActivity.java
+++ app/src/main/java/com/madblackbird/blackbird/activity/HomeActivity.java
@@ -3,7 +3,6 @@ package com.madblackbird.blackbird.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,8 +10,6 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
