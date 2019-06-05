b877598f9ffb558470d407d6a60a09b63c112597
Julen Bujanda <21701719@live.uem.es>
2019-06-05 15:13:24 +0200

    OTPTime class

---
+++ app/src/main/java/com/madblackbird/blackbird/dataClasses/OTPTime.java
@@ -0,0 +1,67 @@
+package com.madblackbird.blackbird.dataClasses;
+
+import android.util.Log;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+public class OTPTime implements Serializable {
+
+    private int dayOfMonth;
+    private int month;
+    private int year;
+    private int hour;
+    private int minute;
+
+    public OTPTime() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date(System.currentTimeMillis()));
+        dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+        month = cal.get(Calendar.MONTH);
+        year = cal.get(Calendar.YEAR);
+        hour = cal.get(Calendar.HOUR_OF_DAY);
+        minute = cal.get(Calendar.MINUTE);
+        Log.d("hor:minute", hour + ":" + minute);
+    }
+
+    public int getDayOfMonth() {
+        return dayOfMonth;
+    }
+
+    public void setDayOfMonth(int dayOfMonth) {
+        this.dayOfMonth = dayOfMonth;
+    }
+
+    public int getMonth() {
+        return month + 1;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+}
