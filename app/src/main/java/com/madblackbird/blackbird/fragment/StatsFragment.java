f8f0ee6cf1b624443f091d8b9c85e7493a87a6f4
Julen Bujanda <21701719@live.uem.es>
2019-06-07 14:57:59 +0200

    Statistics fragment

---
+++ app/src/main/java/com/madblackbird/blackbird/fragment/StatsFragment.java
@@ -0,0 +1,37 @@
+package com.madblackbird.blackbird.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.madblackbird.blackbird.R;
+
+public class StatsFragment extends Fragment {
+
+
+    public StatsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_stats, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private double co2EmissionsGrams(double kilometers) {
+        return kilometers * 251;
+    }
+
+}
