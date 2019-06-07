e921e51d250ed3350d17a8faab72b3361a651c66
Julen Bujanda <21701719@live.uem.es>
2019-06-07 15:15:17 +0200

    Load distance and emissions statistics

--- app/src/main/java/com/madblackbird/blackbird/fragment/StatsFragment.java
+++ app/src/main/java/com/madblackbird/blackbird/fragment/StatsFragment.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.madblackbird.blackbird.R;
+import com.madblackbird.blackbird.service.TripDatabaseService;
+
+import butterknife.BindView;
 
 public class StatsFragment extends Fragment {
 
+    @BindView(R.id.lbl_travelled_kilometers)
+    TextView lblTravelledKilometers;
+    @BindView(R.id.lbl_saved_emissions)
+    TextView lblSavedEmissions;
+
+    private TripDatabaseService tripDatabaseService;
 
     public StatsFragment() {
 
@@ -27,10 +37,15 @@ public class StatsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        tripDatabaseService = new TripDatabaseService();
+        tripDatabaseService.getDistance(distance -> {
+            lblTravelledKilometers.setText(String.valueOf(Math.round(distance)));
+            lblSavedEmissions.setText(String.valueOf(co2EmissionsKm(distance)));
+        });
     }
 
-    private double co2EmissionsGrams(double kilometers) {
-        return kilometers * 251;
+    private long co2EmissionsKm(double kilometers) {
+        return Math.round((kilometers * 251) / 1000);
     }
 
 }
