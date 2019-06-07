370acabff1263cc23c429bbeffb000928efae302
Julen Bujanda <21701719@live.uem.es>
2019-06-07 15:03:45 +0200

    Extract string resource, gentrify

--- app/src/main/java/com/madblackbird/blackbird/fragment/StatsFragment.java
+++ app/src/main/java/com/madblackbird/blackbird/fragment/StatsFragment.java
@@ -1,14 +1,13 @@
 package com.madblackbird.blackbird.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.madblackbird.blackbird.R;
 
